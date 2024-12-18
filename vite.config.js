a4890b9a2d2a277cfa5dcd39d176ebd76036f2b0
AgFelix <marcelo12543@hotmail.com>
2024-12-18 12:20:00 -0300

    🥅 err: tratamento de erros

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-
   build: {
-    outDir: 'dist',
-    rollupOptions: {
-      input: '/public/index.html',  // Certifique-se de que isso está correto
-    },
+    outDir: 'dist', // A pasta de saída da build
+  },
+  server: {
+    open: true, // Abrir no navegador ao iniciar
   },
 });
-
