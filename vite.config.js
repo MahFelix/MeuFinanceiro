bacc4ba873f437c53ca0a5a7d0558344177ec899
AgFelix <marcelo12543@hotmail.com>
2024-12-18 12:27:53 -0300

    🥅 err: tratamento de erros

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,14 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: 'dist', // A pasta de saída da build
+    outDir: 'dist',
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['react', 'react-dom'], // Divide React em um chunk separado
+        },
+      },
+    }, // A pasta de saída da build
   },
   server: {
     open: true, // Abrir no navegador ao iniciar
